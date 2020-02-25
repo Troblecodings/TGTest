8e6d1828e388ffcc07b1f00e9e33b06093eff258
MrTroble <nifroh56@gmail.com>
2020-02-25 12:51:36 +0100

    Test runnable

--- Template/TGMain.cpp
+++ Template/TGMain.cpp
@@ -12,12 +12,18 @@ using namespace tge::tex;
 int main() {
 	initEngine();
 
+	playercontroller = [](Input* in) {};
+
+	TopDownCamera camera;
+	camera.positionx = 0;
+	camera.positiony = 0;
+	camera.screenwidth = tge::win::mainWindowWidth;
+	camera.screenheight = tge::win::mainWindowHeight;
+	setTopDownCamera(&camera);
+
 	Map map;
 	loadResourceFile("test.tgr", &map);
-	// This isn't production code ... I hope you did not think it is ...
-	for_each(map.textures.begin(), map.textures.end(), [](TextureInputInfo in) { printf("X = %i\nY = %i\nComp = %i", in.x, in.y, in.comp); });
-	for_each(map.materials.begin(), map.materials.end(), [](Material in) { printf("color = [%f, %f, %f, %f]\ndiffuseTexture = %i", in.color.r, in.color.g, in.color.b, in.color.a, in.diffuseTexture); });
-	for_each(map.actors.begin(), map.actors.end(), [](ActorInputInfo in) { printf("pProperties.material = %i\npProperties.layer = %i", in.pProperties.material, in.pProperties.layer); });
+
 	startTGEngine();
 	return 0;
 }
